c824451eea60707da96bfa5edefb8039a21c2b33
Mahdi Rezayi <mahdi.rezzai93@gmail.com>
2021-07-03 13:09:35 +0430

    next-auth for login and authentication

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,42 @@
+import { connectToDatabase } from '../../../lib/db'
+import { hashPassword } from '../../../lib/auth'
+
+async function handler(req, res) {
+  if(req.method === 'POST') {
+    const { email, password } = req.body
+
+    if(
+      !email ||
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7 
+    ) {
+      res.status(422).json({ message: 'Invalid input - password should also be at least 7 characters logn.' })
+      return
+    }
+  
+    const client = await connectToDatabase()
+  
+    const db = client.db()
+
+    const existingUser = await db.collection('users').findOne({ email })
+
+    if(existingUser) {
+      res.status(422).json({ message: 'User exists already!' })
+      client.close()
+      return
+    }
+  
+    const hashedPassword = await hashPassword(password)
+  
+    const result = await db.collection('users').insertOne({
+      email,
+      password: hashedPassword
+    })
+  
+    res.status(201).json({ message: 'Created user!' })
+    client.close()
+  }
+}
+
+export default handler
---
+++ pages/auth.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import { getSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import AuthForm from '../components/auth/auth-form';
+
+function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    getSession().then(session => {
+      if(session) {
+        router.replace('/')
+      } else {
+        setIsLoading(false)
+      }
+    })
+  }, [router])
+
+  if(isLoading) {
+    return (
+      <p style={{textAlign: 'center'}}>Loading...</p>
+    )
+  }
+
+  return (
+    <AuthForm />
+  )
+}
+
+export default AuthPage;
